37bdd6bb8a5bfaac9ab95e8353b0ec41789f6ef3
Viktar Tsimashevich <viktar.tsimashevich@gmail.com>
2021-02-05 13:16:18 +0300

    convert parquet => csv

---
+++ pqt2csv.py
@@ -0,0 +1,40 @@
+import sys
+import os
+import glob
+import pandas as pd
+
+
+def show_help():
+    print("python pqt2csv.py [file | dir]")
+    print("where file - parquet file name")
+    print("      dir - all parquet files in the directory")
+
+
+def get_csv_name(file):
+    return "{}.csv".format(file)
+
+
+def process(path):
+    if os.path.isdir(path):
+        os.chdir(path)
+        for file in glob.glob("*.parquet"):
+            convert(file, get_csv_name(file))
+    elif os.path.isfile(path):
+        convert(path, get_csv_name(file))
+
+
+def convert(source, destination):
+    if os.path.exists(source):
+        df = pd.read_parquet(source)
+        df.to_csv(destination)
+        print("File '{}' processed".format(source))
+
+
+if len(sys.argv) < 2:
+    show_help()
+    sys.exit(1)
+
+path = sys.argv[1]
+
+if __name__ == "__main__":
+    process(path)
